c1d93ce68d99ba7606310737fdd81bdd74501403
Shilcof <robert.shilcof@gmail.com>
2020-10-25 20:40:32 +0000

    commit tree 5ba9a4c780e9073b196684f950757a8dc30a4379

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,57 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = (name)
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{ |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by{ |song| song.name }
+  end
+
+  def self.detail_from_filename(filename)
+    details = filename.split(" - ")
+    artist, name = details[0], details[1].gsub(".mp3","")
+  end
+
+  def self.new_from_filename(filename)
+    artist, name = self.detail_from_filename(filename)
+    song = self.new_by_name(name)
+    song.artist_name = artist
+    song
+  end
+
+  def self.create_from_filename(filename)
+    artist, name = self.detail_from_filename(filename)
+    song = self.create_by_name(name)
+    song.artist_name = artist
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
